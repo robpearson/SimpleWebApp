cf8bf506a639ef1451b33725d830f93f9d2688ed
robpearson <robpearson@fastmail.net>
2015-08-11 14:07:08 +1000

    Initial commit.

---
+++ RobPearson.SimpleWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RobPearson.SimpleWebApp.MvcApplication" Language="C#" %>
---
+++ RobPearson.SimpleWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RobPearson.SimpleWebApp.Startup))]
+namespace RobPearson.SimpleWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
